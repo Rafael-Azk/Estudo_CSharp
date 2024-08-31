51ae4ee2fbecdd506dab5208c7ea8bdeb01d0114
Azk <bloodrafa@hotmail.com>
2024-08-31 02:13:22 -0300

    PC_2

--- DelegateLambdaLinq/Program.cs
+++ DelegateLambdaLinq/Program.cs
@@ -148,21 +148,206 @@
 //DELEGATE FUNC:
 //Recebe até 16 parametros, mais um retorno. EX <int, int> (Param e retorno).
 
-double num = 10;
-double num2 = 20;   
+//double num = 10;
+//double num2 = 20;   
 
-Func<double, double> Raiz = delegate (double x)//Metodo anonimo comum.
+//Func<double, double> Raiz = delegate (double x)//Metodo anonimo comum.
+//{
+//    return Math.Sqrt(x);
+//};
+//double res = Raiz(num2);
+//Console.WriteLine($"Raiz é {res}");
+
+
+//Func<double, double> RaizQ = x => Math.Sqrt(x); //Metodo anonimo com Lambda.
+//double result = RaizQ(num);
+//Console.WriteLine($"\nRaiz Q de {num} é {result}");
+
+//--------------------------------------EXERCICIO------------------------
+
+//var listaPessoas = new List<Pessoa>()
+//{
+//    new Pessoa("Ana", 21),
+//    new Pessoa("Paula", 31),
+//    new Pessoa("Lorena", 18),
+//    new Pessoa("Bia", 17),
+//    new Pessoa("Kétlin", 20)
+//};
+
+//Console.WriteLine("Lista:");
+//Action<Pessoa> imprimePessoa = p => Console.WriteLine($"Nome: {p.nome} - Idade: {p.idade}");
+//listaPessoas.ForEach(imprimePessoa);
+////---------------------
+//Console.WriteLine("\nMaiores de 18:");
+//Predicate<Pessoa> deMaior = p => p.idade > 18;
+//var maiores18 = listaPessoas.FindAll(deMaior);
+//maiores18.ForEach(imprimePessoa);
+////---------------------
+//Console.WriteLine("\n O mais velho:");
+//Func<Pessoa, int> maisVelho = p => p.idade;
+//var pessoaMaisVelha = listaPessoas.MaxBy(maisVelho);//Melhor metodo.
+//Console.WriteLine($"Mais velho: {pessoaMaisVelha.nome} - {pessoaMaisVelha.idade} anos");
+
+//int idadeMax = listaPessoas.Max(maisVelho); //Metodo tradicional.
+//var pessoaIdadeMax = listaPessoas.Find(p =>  p.idade == idadeMax);
+//Console.WriteLine($"Mais velho: {pessoaIdadeMax.nome} - {pessoaIdadeMax.idade} anos");
+
+
+//public class Pessoa
+//{
+//    public Pessoa(string? nome, int idade)
+//    {
+//        this.nome = nome;
+//        this.idade = idade;
+//    }
+
+//    public string? nome { get; set; }
+//    public int idade { get; set; }
+//}
+
+//----------------------------------------------------------------------------
+//EVENTOS:
+
+//var pedido = new Pedido();
+
+////Inscrevendo os assinantes do evento:
+//pedido.OnCriarPedido += Email.Send;
+//pedido.OnCriarPedido += SMS.Send;
+
+////Acionando metodo e os eventos:
+//pedido.CriarPedido();
+
+//class Pedido
+//{
+//    public event PedidoEvent? OnCriarPedido;
+//    public void CriarPedido()
+//    {
+//        Console.WriteLine("Pedido criado");
+//        if (OnCriarPedido != null)
+//            OnCriarPedido();
+//    }
+//}
+
+//class Email
+//{
+//    public static void Send()
+//    {
+//        Console.WriteLine("Enviando Email");
+//    }
+//}
+
+//class SMS
+//{
+//    public static void Send()
+//    {
+//        Console.WriteLine("Enviando SMS");
+//    }
+//}
+
+//delegate void PedidoEvent();
+
+//--------------------EVENTHANDLER---------------------
+
+//var pedido = new Pedido();
+
+////Inscrevendo os assinantes do evento:
+//pedido.OnCriarPedido += Email.Send;
+//pedido.OnCriarPedido += SMS.Send;
+
+////Acionando metodo e os eventos:
+//pedido.CriarPedido();
+
+//class Pedido
+//{
+//    public event EventHandler? OnCriarPedido;
+//    public void CriarPedido()
+//    {
+//        Console.WriteLine("Pedido criado");
+//        if (OnCriarPedido != null)
+//            OnCriarPedido(this, EventArgs.Empty);
+//    }
+//}
+
+//class Email
+//{
+//    public static void Send(object? sender, EventArgs e)
+//    {
+//        Console.WriteLine("Enviando Email");
+//    }
+//}
+
+//class SMS
+//{
+//    public static void Send(object? sender, EventArgs e)
+//    {
+//        Console.WriteLine("Enviando SMS");
+//    }
+//}
+
+//------------EVENTHANDLER COM ARGS (EVENTHANDLER<TARGS>
+
+//var pedido = new Pedido();
+
+////Inscrevendo os assinantes do evento:
+//pedido.OnCriarPedido += Email.Send;
+//pedido.OnCriarPedido += SMS.Send;
+
+////Acionando metodo e os eventos:
+//pedido.CriarPedido("blablabla@hotmail.com", 13955668874);
+
+
+
+
+//class PedidoEvento : EventArgs
+//{
+//    public string? email { get; set; }
+//    public long telefone { get; set; }
+//}
+//class Pedido
+//{
+//    public event EventHandler<PedidoEvento>? OnCriarPedido;
+//    public void CriarPedido(string email, long fone)
+//    {
+//        Console.WriteLine("Pedido criado");
+//        if (OnCriarPedido != null)
+//            OnCriarPedido(this, new PedidoEvento { email = email, telefone = fone });
+//    }
+//}
+
+//class Email
+//{
+//    public static void Send(object? sender, PedidoEvento e)
+//    {
+//        Console.WriteLine($"Enviando Email para - {e.email}");
+//    }
+//}
+
+//class SMS
+//{
+//    public static void Send(object? sender, PedidoEvento e)
+//    {
+//        Console.WriteLine($"Enviando SMS para: {e.telefone}");
+//    }
+//}
+
+//-------------------------------------------------------------------------
+//METODO DE EXTENSÃO:
+
+string texto = "Rafael de Almeida";
+string textoInvertido = texto.InverteString();
+
+Console.WriteLine(textoInvertido);
+
+
+public static class StringExtensions
 {
-    return Math.Sqrt(x);
-};
-double res = Raiz(num2);
-Console.WriteLine($"Raiz é {res}");
-
-
-Func<double, double> RaizQ = x => Math.Sqrt(x); //Metodo anonimo com Lambda.
-double result = RaizQ(num);
-Console.WriteLine($"\nRaiz Q de {num} é {result}");
-
+    public static string InverteString(this string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
 
 
 
