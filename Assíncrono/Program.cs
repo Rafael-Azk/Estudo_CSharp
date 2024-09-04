0a895a0f757bae94a3ed41771a68bd98591d4828
Azk <bloodrafa@hotmail.com>
2024-09-04 05:21:33 -0300

    PC_2

---
+++ Assíncrono/Program.cs
@@ -0,0 +1,96 @@
+﻿//PORGRAMAÇÃO ASSÍNCRONA:
+
+//---------------------EXEMPLO SÍNCRONO-----------
+
+//int espera = 4000; //4k milisegundos = 4 segundos.
+
+//Console.WriteLine("Tecle algo para iniciar...");
+//Console.ReadLine();
+
+//RealizaTarefa(espera); /* O código abaixo não sera executado enquanto o método
+//                        * não finalizar sua tarefa. (Thread bloqueada)*/
+
+//Console.WriteLine($"\n Tempo gasto = {espera / 1000} segundos"); 
+//Console.WriteLine("\n ### Fim do processamento...");
+
+
+//static void RealizaTarefa(int tempo)
+//{
+//    Console.WriteLine("\nIniciando tarefa...");
+//    Thread.Sleep(tempo); //Codigo para sincrono.
+//    /*Task.Delay(TimeSpan.FromSeconds(tempo));*/ //Código para assincrono.
+//    Console.WriteLine("\nTarefa concluída...");
+//}
+
+//------------------------ASSÍNCRONO--------------------------------
+/* Task: Representa uma execução assíncrona. (Task/Task<T> - ValueTask/ValueTask<T> - Void).
+ * Await: Aguarda uma operação.
+ * Async: Sinaliza que um método é assíncrono.*/
+
+//-------------------Sintaxe básica---------------------
+//Task:
+//await MetodoAsync(); //Chamada do metodo.
+//static async Task MetodoAsync()
+//{
+//    await Task.Delay(1000);
+//    Console.WriteLine("Operação assíncrona concluíida");
+//}
+
+//Task<T>:
+//var result = await MetodoAsync();
+//static async Task<int> MetodoAsync()
+//{
+//    await Task.Delay(1000);
+//    Console.WriteLine("Operação assíncrona concluíida");
+//    return 1;
+//}
+
+//--------------------------Exemplo:--------------
+
+Console.WriteLine("## Café da manhã assíncrono...##");
+CafeDaManhaAsync();
+
+Console.ReadKey();
+
+static async void CafeDaManhaAsync()
+{
+    Console.WriteLine("Preparar café...");
+    var tarefaCafe = PrepararCafeAsync();
+    Console.WriteLine("Preparar pão...");
+    var tarefaPao = PrepararPaoAsync();
+
+    var cafe = await tarefaCafe;
+    var pao = await tarefaPao;
+
+    ServirCafe(cafe, pao);
+    ;
+}
+static void ServirCafe(Cafe cafe, Pao pao)
+{
+    Console.WriteLine("Servindo café da manhã...");
+    Thread.Sleep(3000);
+    Console.WriteLine("Café servido...");
+
+}
+
+static async Task<Cafe> PrepararCafeAsync()
+{
+    Console.WriteLine("Fervendo água...");
+    await Task.Delay(2000);
+    Console.WriteLine("Coando café...");
+    await Task.Delay(2000);
+    Console.WriteLine("Adoçando açucar...");
+    return new Cafe();
+}
+static async Task<Pao> PrepararPaoAsync()
+{
+    Console.WriteLine("Partir o pão...");
+    await Task.Delay(2000);
+    Console.WriteLine("Passar manteiga...");
+    await Task.Delay(2000);
+    Console.WriteLine("Tostar pão na chapa...");
+    return new Pao();
+}
+
+public class Cafe { }
+public class Pao { }
---
+++ Serializacao/Program.cs
@@ -0,0 +1,127 @@
+﻿//SERIALIZAÇÃO:
+
+//------------------------BINÁRIA (OBSOLETA)-----------------------------
+
+//using System.Runtime.Serialization.Formatters.Binary;
+
+//var alu1 = new Aluno("Maria", "kkk@hotmail.com", 18);
+//var caminhoArquivo = @"C:\Users\Usuario\Desktop\AlunoSerial.bin";
+//using (FileStream fs = new FileStream(caminhoArquivo, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+//{
+//    var bf = new BinaryFormatter(); //Obsoleto. Não vai compilar.
+//    bf.Serialize(fs, alu1);
+
+//    Console.WriteLine("\nTecle algo para desserializar...");
+//    Console.ReadKey();
+//    fs.Seek(0, SeekOrigin.Begin);
+//    var alunoDesserializado = (Aluno)bf.Deserialize(fs);
+//    Console.WriteLine(alunoDesserializado.Nome);
+//}
+//Console.WriteLine("Serializado com sucesso...");
+
+
+//[Serializable]
+//public class Aluno
+//{
+//    public string Nome { get; set; }
+//    public string Email { get; set; }
+//    [NonSerialized]
+//    public int Idade;
+
+//    public Aluno(string nome, string email, int idade)
+//    {
+//        Nome = nome;
+//        Email = email;
+//        Idade = idade;
+//    }
+//}
+
+//-----------------------------------XML----------------------
+//using System.Xml.Serialization;
+
+//var alu1 = new Aluno("Maria", "kkk@hotmail.com", 18);
+//var caminhoArquivo = @"C:\Users\Usuario\Desktop\AlunoSerial.bin";
+
+//XmlSerializer serializar = new XmlSerializer(typeof(Aluno));
+
+//using (StreamWriter writer = new StreamWriter(caminhoArquivo))
+//{
+//    serializar.Serialize(writer, alu1);
+//}
+//Console.WriteLine("Serializado para XML com sucesso....");
+//Console.WriteLine("\n Tecle para desserializar....");
+//Console.ReadKey();
+
+//using (StreamReader reader = new StreamReader(caminhoArquivo))
+//{
+//    var aluno = (Aluno)serializar.Deserialize(reader);
+
+//    Console.WriteLine($"Aluno desserializado: Nome: {aluno.Nome} - Idade: {aluno.Idade} - " +
+//        $"Email: {aluno.Email}");
+//}
+//Console.ReadKey();
+
+
+
+//public class Aluno
+//{
+//    public string Nome { get; set; }
+//    public string Email { get; set; }
+//    public int Idade;
+
+//    public Aluno() { } /*Construtor padrão necessário para serializar XML.
+//                        * Não necessário em caso de classe sem construtor adicional.*/
+
+//    public Aluno(string nome, string email, int idade)
+//    {
+//        Nome = nome;
+//        Email = email;
+//        Idade = idade;
+//    }
+//}
+
+//-------------------------------------JASON-----------------------------
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var alu1 = new Aluno("Maria", "kkk@hotmail.com", 18);
+var caminhoArquivo = @"C:\Users\Usuario\Desktop\AlunoSerial.json";
+
+using FileStream stream = new FileStream(caminhoArquivo,
+                                         FileMode.OpenOrCreate, FileAccess.ReadWrite);
+{
+    JsonSerializer.Serialize(stream, alu1);
+}
+
+Console.WriteLine("Serializado Json com sucesso...");
+Console.ReadKey();
+/*OBS: Precisa desserializar sem a serialização em uso. Separar por projetos Class*/
+string conteudoJson = File.ReadAllText(caminhoArquivo);
+var aluno = JsonSerializer.Deserialize<Aluno>(conteudoJson);
+Console.WriteLine($"\nAluno desserializado: Nome: {aluno.Nome} - Idade: {aluno.Idade} - " +
+                                                $"Email: {aluno.Email}");
+
+
+public class Aluno
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    //public int Idade; /*Não será serializada pois naõ tem 
+    //                   * propriedade "Get/Set". Também existe outra forma, "JsonIgnore".
+
+    [JsonIgnore] // Não vai serializar idade.
+    public int Idade { get; set; }
+
+    public Aluno() { } /*Construtor padrão necessário para serializar XML.
+                        * Não necessário em caso de classe sem construtor adicional.*/
+
+    public Aluno(string nome, string email, int idade)
+    {
+        Nome = nome;
+        Email = email;
+        Idade = idade;
+    }
+}
+
+
+
